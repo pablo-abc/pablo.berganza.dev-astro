4ba6ccf0d809986480a7788256a7f1ee7d17caa2
Pablo Berganza <40573613+pablo-abc@users.noreply.github.com>
2021-07-28 02:26:32 +0200

    feat: add home page

--- astro.config.mjs
+++ astro.config.mjs
@@ -8,9 +8,9 @@ export default {
     sitemap: true,         // Generate sitemap (set to "false" to disable)
   },
   devOptions: {
-    // hostname: 'localhost',  // The hostname to run the dev server on. 
+    // hostname: 'localhost',  // The hostname to run the dev server on.
     // port: 3000,             // The port to run the dev server on.
     // tailwindConfig: '',     // Path to tailwind.config.js if used, e.g. './tailwind.config.js'
   },
-  renderers: [],
+  renderers: ['@astrojs/renderer-svelte'],
 };
---
+++ src/components/Home/Abilities.svelte
@@ -0,0 +1,47 @@
+<script>
+  import { onMount, onDestroy } from 'svelte';
+  import { fade } from 'svelte/transition';
+  const abilities = [
+    ['https://cdn.svgporn.com/logos/javascript.svg', 'JavaScript'],
+    ['https://cdn.svgporn.com/logos/typescript-icon.svg', 'TypeScript'],
+    ['https://cdn.svgporn.com/logos/clojure.svg', 'Clojure'],
+    ['assets/abilities/cljs-white.svg', 'ClojureScript'],
+    ['https://cdn.svgporn.com/logos/python.svg', 'Python'],
+    ['https://cdn.svgporn.com/logos/docker.svg', 'Docker'],
+    ['assets/abilities/nestjs.svg', 'NestJS'],
+    ['assets/abilities/react.svg', 'React'],
+    ['assets/abilities/svelte.svg', 'Svelte'],
+  ];
+  let index = 0;
+  let interval;
+
+  onMount(() => {
+    interval = setInterval(() => {
+      if (index === abilities.length - 1) index = 0;
+      else index += 1;
+    }, 2000);
+  });
+  onDestroy(() => clearInterval(interval));
+</script>
+
+<div aria-hidden="true">
+  {#key index}
+  <img transition:fade src="{abilities[index][0]}" alt="" />
+  {/key}
+</div>
+
+<style>
+  div {
+    position: relative;
+    width: 100%;
+    height: 100%;
+  }
+  img {
+    position: absolute;
+    top: 50%;
+    transform: translateY(-50%);
+    left: 0;
+    width: 100%;
+    height: 100%;
+  }
+</style>
