4a10a863c32ea823db7c10c40528855b801877f7
Pablo Berganza <40573613+pablo-abc@users.noreply.github.com>
2022-02-28 02:01:07 +0100

    style: improve TOC styles

--- src/layouts/copy-button.js
+++ src/layouts/copy-button.js
@@ -1,3 +1,50 @@
+const styleString = `
+    .code-container {
+      position: relative;
+      margin: 8px auto;
+      width: 95%;
+    }
+
+    .code-container__copy-button {
+      opacity: 0.5;
+      padding-left: 0.5rem;
+      padding-right: 0.3rem;
+      padding-top: 0.6rem;
+      padding-bottom: 0.1rem;
+      border-radius: 10px;
+      color: var(--foreground);
+      font-size: 1rem;
+      position: absolute;
+      top: 0.2rem;
+      right: 0.2rem;
+    }
+
+    .code-container__copy-button--copied {
+      color: var(--color-complement-3);
+    }
+
+    .code-container__copy-button--failed {
+      color: var(--color-secondary-1-2);
+    }
+
+    .code-container__copy-button svg {
+      height: 1.5rem;
+    }
+
+    .code-container__copy-button:focus-visible {
+      opacity: 1;
+    }
+
+    .code-container__copy-button:hover, .code-container__copy-button:focus-visible {
+      opacity: 1;
+      background: hsl(231,15%,28%);
+      cursor: pointer;
+    }
+    .code-container__copy-button:active {
+      background: hsl(231,15%,38%);
+    }
+`;
+
 const i18n = {
   en: {
     tooltip: {
@@ -19,6 +66,10 @@ function load() {
   const lang = document.documentElement.lang;
   const messages = i18n[lang];
   const codeBlocks = document.querySelectorAll('pre');
+  const style = document.createElement('style');
+  style.type = 'text/css';
+  style.textContent = styleString;
+  document.head.appendChild(style);
   const copySvg = `
 <svg aria-hidden="true" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
 <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M8 5H6a2 2 0 00-2 2v12a2 2 0 002 2h10a2 2 0 002-2v-1M8 5a2 2 0 002 2h2a2 2 0 002-2M8 5a2 2 0 012-2h2a2 2 0 012 2m0 0h2a2 2 0 012 2v3m2 4H10m0 0l3-3m-3 3l3 3"></path>
@@ -40,10 +91,13 @@ function load() {
     parent.insertBefore(container, block);
     container.appendChild(block);
     block.tabIndex = 0;
-    const buttonContainer = document.createElement('div');
     const copyButton = document.createElement('button');
+    container.appendChild(copyButton);
     const svgContainer = document.createElement('span');
+    copyButton.appendChild(svgContainer);
     const tooltip = document.createElement('div');
+    copyButton.appendChild(tooltip);
+
     tooltip.id = `tooltip-copy-${id}`;
     tooltip.className = 'button-tooltip';
     tooltip.setAttribute('aria-hidden', true);
@@ -51,32 +105,27 @@ function load() {
     tooltip.style.position = 'fixed';
     tooltip.textContent = messages.tooltip.idle;
     svgContainer.innerHTML = copySvg;
-    buttonContainer.appendChild(tooltip);
-    copyButton.appendChild(svgContainer);
-    copyButton.className = 'copy-button';
+    copyButton.className = 'code-container__copy-button';
     copyButton.setAttribute('aria-labelledby', tooltip.id);
-    buttonContainer.className = 'button-container';
-    buttonContainer.appendChild(copyButton);
-    container.appendChild(buttonContainer);
     copyButton.addEventListener('click', async () => {
       try {
         await navigator.clipboard.writeText(textContent).then(() => {
           svgContainer.innerHTML = copiedSvg;
           tooltip.textContent = messages.tooltip.success;
-          buttonContainer.classList.add('copied');
+          copyButton.classList.add('code-container__copy-button--copied');
           setTimeout(() => {
             svgContainer.innerHTML = copySvg;
-            buttonContainer.classList.remove('copied');
+            copyButton.classList.remove('code-container__copy-button--copied');
             tooltip.textContent = messages.tooltip.idle;
           }, 500);
         });
       } catch {
         svgContainer.innerHTML = failedSvg;
         tooltip.textContent = messages.tooltip.failed;
-        buttonContainer.classList.add('failed');
+        copyButton.classList.add('code-container__copy-button--failed');
         setTimeout(() => {
           svgContainer.innerHTML = copySvg;
-          buttonContainer.classList.remove('failed');
+          copyButton.classList.remove('code-container__copy-button--failed');
           tooltip.textContent = messages.tooltip.idle;
         }, 500);
       }
@@ -88,10 +137,13 @@ function load() {
       tooltip.style.top = `${buttonRect.top + buttonRect.height / 2}px`;
       tooltip.style.transform = 'translateY(-50%)';
     }
+    repositionTooltip();
+
     let interval = null;
     function showTooltip() {
       tooltip.style.visibility = 'visible';
       if (interval) clearInterval(interval);
+      repositionTooltip();
       interval = setInterval(repositionTooltip, 10);
     }
     function hideTooltip() {
@@ -100,7 +152,6 @@ function load() {
       clearInterval(interval);
       interval = null;
     }
-    repositionTooltip();
     copyButton.addEventListener('mouseenter', showTooltip);
     copyButton.addEventListener('focusin', () => {
       if (!copyButton.classList.contains('focus-visible')) return;
@@ -119,8 +170,6 @@ function load() {
       }, 700);
     });
     document.addEventListener('visibilitychange', hideTooltip);
-    const resizeObserver = new ResizeObserver(repositionTooltip);
-    resizeObserver.observe(tooltip);
     id += 1;
   }
 }
