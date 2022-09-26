4a689399c27910cafdfe299b5c435984ac458df7
Pablo Berganza <40573613+pablo-abc@users.noreply.github.com>
2022-09-26 23:29:52 +0200

    build: upgrade to astro 1.0

---
+++ src/pages/blog/rss.xml.js
@@ -0,0 +1,32 @@
+import rss from '@astrojs/rss';
+
+const postsImport = import.meta.glob('../../posts/en/*.md', { eager: true });
+const posts = Object.values(postsImport);
+
+export const get = () =>
+  rss({
+    title: 'Blog | Pablo Berganza',
+    stylesheet: true,
+    description: 'Articles about web dev among other things',
+    customData: '<language>en-us</language>',
+    site: import.meta.env.SITE,
+    items: posts
+      .filter((post) => {
+        if (import.meta.env.PROD) {
+          return post.frontmatter.published;
+        }
+        return true;
+      })
+      .sort((a, b) => {
+        return (
+          new Date(b.frontmatter.created).getTime() -
+          new Date(a.frontmatter.created).getTime()
+        );
+      })
+      .map((item) => ({
+        title: item.frontmatter.title,
+        description: item.frontmatter.description,
+        link: item.file.replace('/src/posts/en', '/blog').replace('.md', ''),
+        pubDate: item.frontmatter.created,
+      })),
+  });
---
+++ src/pages/es/blog/rss.xml.js
@@ -0,0 +1,32 @@
+import rss from '@astrojs/rss';
+
+const postsImport = import.meta.glob('../../../posts/es/*.md', { eager: true });
+const posts = Object.values(postsImport);
+
+export const get = () =>
+  rss({
+    title: 'Blog | Pablo Berganza',
+    stylesheet: true,
+    description: 'Articulos sobre desarrollo web entre otras cosas',
+    customData: '<language>es-sv</language>',
+    site: import.meta.env.SITE,
+    items: posts
+      .filter((post) => {
+        if (import.meta.env.PROD) {
+          return post.frontmatter.published;
+        }
+        return true;
+      })
+      .sort((a, b) => {
+        return (
+          new Date(b.frontmatter.created).getTime() -
+          new Date(a.frontmatter.created).getTime()
+        );
+      })
+      .map((item) => ({
+        title: item.frontmatter.title,
+        description: item.frontmatter.description,
+        link: item.file.replace('/src/posts/es', '/es/blog').replace('.md', ''),
+        pubDate: item.frontmatter.created,
+      })),
+  });
