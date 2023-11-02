7aacb91bc3eaecc0def60f3c92db8eec9f7e9d9a
pablo-abc <pablo-abc@users.noreply.github.com>
2023-11-02 20:53:34 +0000

    deploy: 23f6e3c18a425c21877632e44ef5a57d40240123

--- _astro/hoisted.4834e992.js
+++ _astro/hoisted.4834e992.js
@@ -108,6 +108,9 @@ import"https://unpkg.com/yt-vid@0.0.5/dist/yt-vid/yt-vid.js";const Te=`
             <label part="label" for="instance">Instance:</label>
             <input
               type="text"
+              autocorrect="off"
+              autocapitalize="off"
+              spellcheck="false"
               id="instance"
               name="instance"
               part="input"
@@ -151,6 +154,8 @@ import"https://unpkg.com/yt-vid@0.0.5/dist/yt-vid/yt-vid.js";const Te=`
       background-color: transparent;
       border: none;
       cursor: pointer;
+      margin: 0;
+      padding: 0;
     }
 
     dialog {
