f37ae0844bc5a88b196f4aea3b8017b3813d16f9
pablo-abc <pablo-abc@users.noreply.github.com>
2024-02-08 20:08:36 +0000

    deploy: 2c59c6ba25094a2d5668156c88aaf12a1ceabbe5

---
+++ _astro/index.Q8HUYPWv.js
@@ -0,0 +1,1 @@
+import{w as n}from"./index.dyl9NMoU.js";function r(t,{delay:o=0,duration:e=400,easing:i=n}={}){const a=+getComputedStyle(t).opacity;return{delay:o,duration:e,easing:i,css:c=>`opacity: ${c*a}`}}export{r as f};
